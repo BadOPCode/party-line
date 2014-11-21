e81ea89862886b29080d835e3f132245b1b3b653
BadOPCode <shawn@wmsi.co>
2014-11-21 07:22:06 +0000

    Initial commit

---
+++ info.md
@@ -0,0 +1,21 @@
+Party Line Bus Module
+=====================
+
+Author
+------
+
+Shawn Rapp 2014-10-09
+
+
+Description
+-----------
+
+Party Line creates a bus that provides internal and external API links of communication.
+
+
+How To Use
+----------
+
+
+API Function List
+-----------------
---
+++ tests/test-pl.js
@@ -0,0 +1,19 @@
+var partyline = require('../lib/index.js');
+  
+var test  = partyline.addWorker();
+
+test.setContext('testing');
+test.setEventCallback(function(packet){
+  console.log(packet.message);
+});
+test.emit({context:"8675309", from:"testing"});
+
+var testfork = new partyline.ForkLink('sh', function(packet){ 
+    console.log(packet);
+});
+testfork.package_output = true;
+testfork.unpackage_input = true;
+testfork.startService();
+testfork.send({data:'ls\n'});
+testfork.send({data:'ls /\n'});
+testfork.send({data:'exit\n'});
